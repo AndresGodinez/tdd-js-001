d688cecb3bbffdb0bca32abf6fc4882346b4846d
AGo <ing.a.godinez@gmail.com>
2021-09-10 12:40:40 -0500

    add validator on project

--- routes/validation.js
+++ routes/validation.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const createError = require('http-errors');
+const Validator = require('validatorjs')
 
 router.post('/', function (req, res, next) {
     let {warranty} = req.body;
     let value;
+
+    let rules = {
+        warranty: 'required|numeric|max:4',
+    };
+
+    let validation = new Validator({warranty}, rules)
+    
+    if (validation.fails()){
+        res.json(validation.errors);
+    }
+
     switch (String(warranty)) {
         case '0':
             value=151;
